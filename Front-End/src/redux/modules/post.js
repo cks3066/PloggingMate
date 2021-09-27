99a9a11f4756280fdd7ce04b90f216d869db326e
On <cks3066@naver.com>
2021-09-27 20:42:50 +0900

    feat : 포스트 이미지 작업

--- Front-End/src/redux/modules/post.js
+++ Front-End/src/redux/modules/post.js
@@ -57,32 +57,54 @@ const applyPost = (postId) => {
   };
 };
 
-const addPost = (title, address, location, time, content, history) => {
+const addPost = (title, address, location, time, content, file, history) => {
   return function (dispatch, getState) {
+    const formData = new FormData();
+    var contents = {
+      name: "제102호 어린이공원",
+      address: "경기도 용인시 수지구 죽전동 1484번지 일원",
+    };
+    formData.append("file", file);
+    formData.append("content", contents);
     axios
-      .post(
-        `http://localhost:8080/app/posts/`,
-        {
-          contents: title,
-          reservedAt: time,
-          totalApplyCount: 5,
-          name: location,
-          address: address,
+      .post("http://localhost:8080/app/park", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "X-ACCESS-TOKEN": getState().user.jwt,
+          withCredentials: true,
         },
-        {
-          headers: {
-            "X-ACCESS-TOKEN": getState().user.jwt,
-          },
-        }
-      )
-      .then(async (res) => {
+      })
+      .then((res) => {
         console.log(res);
-        history.goBack();
       })
       .catch((error) => {
         console.dir(error);
       });
   };
+  //   axios
+  //     .post(
+  //       `http://localhost:8080/app/posts/`,
+  //       {
+  //         contents: title,
+  //         reservedAt: time,
+  //         totalApplyCount: 5,
+  //         name: location,
+  //         address: address,
+  //       },
+  //       {
+  //         headers: {
+  //           "X-ACCESS-TOKEN": getState().user.jwt,
+  //         },
+  //       }
+  //     )
+  //     .then(async (res) => {
+  //       console.log(res);
+  //       history.goBack();
+  //     })
+  //     .catch((error) => {
+  //       console.dir(error);
+  //     });
+  // };
 };
 // 리듀서
 export default createReducer(initialState, {
